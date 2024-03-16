6519c05c737a825bda235e78a41cefff97993ceb
Antonin <alandeau@charles-poncet.net>
2024-03-16 11:11:37 +0100

    env variables fe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    BACKEND_URL: process.env.BACKEND_URL 
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
